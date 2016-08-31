1c24391ca7bacce14696b86275eaf14c3b7a20e2
toastal <toastal@posteo.net>
2016-08-31 01:05:01 -0600

    code samples

--- Either.elm
+++ Either.elm
@@ -56,6 +56,9 @@ type Either a b
 {-| Apply a function to an `Either`. If the argument is `Right`, it
 will be converted.  If the argument is an `Left`, the same left value
 will propogate through.
+
+    map ((+) 1) <| Left "Hello" == Left "Hello"
+    map ((+) 1) <| Right 2 == Right 3
 -}
 map : (a -> b) -> Either x a -> Either x b
 map f e =
@@ -70,6 +73,11 @@ map f e =
 {-| Apply a function to two eithers, if both arguments are `Right`.
 If not, the first argument which is a `Left` will propagate through.
 Also known as `liftA2`.
+
+    map2 (+) (Left "Hello") <| Left "World" == Left "Hello"
+    map2 (+) (Left "Hello") <| Right 3 == Left "Hello"
+    map2 (+) (Right 2) <| Left "World" == Left "World"
+    map2 (+) (Right 2) <| Right 3 == Right 5
 -}
 map2 : (a -> b -> c) -> Either x a -> Either x b -> Either x c
 map2 f e e' =
@@ -103,6 +111,9 @@ map3 f e e' e'' =
 
 
 {-| Apply a function to the `Left` of an `Either`.
+
+    mapLeft ((+) 1) <| Left 2 == Left 3
+    mapLeft ((+) 1) <| Right 2 == Right 2
 -}
 mapLeft : (a -> b) -> Either a x -> Either b x
 mapLeft f e =
@@ -123,6 +134,9 @@ mapRight =
 
 {-| Apply the first argument function to a `Left` and the second
 argument function to a `Right` of an `Either`.
+
+    mapBoth (flip (++) "!") ((+) 1) <| Left "Hello" == Left "Hello!"
+    mapBoth (flip (++) "!") ((+) 1) <| Right 2 == Right 3
 -}
 mapBoth : (a -> b) -> (c -> d) -> Either a c -> Either b d
 mapBoth f g e =
@@ -136,6 +150,9 @@ mapBoth f g e =
 
 {-| Apply a function to `Right` value. If argument was a `Left` use the
 default value. Equivalent to `Either.map >> Either.fromRight`.
+
+    mapDefault 99 ((+) 1) <| Left "Hello" == 99
+    mapDefault 99 ((+) 1) <| Right 2 == 3
 -}
 mapDefault : c -> (b -> c) -> Either x b -> c
 mapDefault d f e =
@@ -153,6 +170,8 @@ mapDefault d f e =
 
 {-| Create a `singleton` from a value to an `Either` with a `Right`
 of the same type.  Also known as `pure`.
+
+    singleton 2 == Right 2
 -}
 singleton : b -> Either x b
 singleton =
@@ -162,6 +181,11 @@ singleton =
 {-| Apply the function that is inside `Either` to a value that is inside
 `Either`. Return the result inside `Either`. If one of the `Either`
 arguments is `Left x`, return `Left x`. Also known as `apply`.
+
+    Left "Hello" `andMap` Left "World" == Left "Hello"
+    Left "Hello" `andMap` Right 2 == Left "Hello"
+    Right ((+) 1) `andMap` Left "World" == Left "World"
+    Right ((+) 1) `andMap` Right 2 == Right 3
 -}
 andMap : Either x (a -> b) -> Either x a -> Either x b
 andMap e e' =
@@ -179,6 +203,9 @@ andMap e e' =
 
 {-| Chain together in many computations that will stop computing if
 a chain is on a `Left`. Also known as `bind`.
+
+    Left "Hello" `andThen` ((+) 1 >> Right) == Left "Hello"
+    Right 2 `andThen` ((+) 1 >> Right) == Right 3
 -}
 andThen : Either x a -> (a -> Either x b) -> Either x b
 andThen e f =
@@ -196,6 +223,8 @@ andThen e f =
 
 
 {-| Converts a `List` of `Either a x` to a List of `a`.
+
+    lefts [ Left "Hello", Right 2 ] == [ "Hello" ]
 -}
 lefts : List (Either a x) -> List a
 lefts =
@@ -212,6 +241,8 @@ lefts =
 
 
 {-| Converts a `List` of `Either x b` to a List of `b`.
+
+    rights [ Left "Hello", Right 2 ] == [ 2 ]
 -}
 rights : List (Either x b) -> List b
 rights =
@@ -229,6 +260,8 @@ rights =
 
 {-| Converts a `List` of `Either a b`, into a tuple2 where
 the first value is a `List a` and the second is `List b`.
+
+    partition [ Left "Hello", Right 2 ] == ( [ "Hello" ], [ 2 ] )
 -}
 partition : List (Either a b) -> ( List a, List b )
 partition =
@@ -250,6 +283,9 @@ partition =
 
 
 {-| `Maybe` get the `Right` side of an `Either`.
+
+    toMaybe <| Left "World" == Nothing
+    toMaybe <| Right 2 == Just 2
 -}
 toMaybe : Either x b -> Maybe b
 toMaybe e =
@@ -262,6 +298,9 @@ toMaybe e =
 
 
 {-| `Maybe` get the `Left` side of an `Either`.
+
+    leftToMaybe <| Left "World" == Just "World"
+    leftToMaybe <| Right 2 == Nothing
 -}
 leftToMaybe : Either a x -> Maybe a
 leftToMaybe e =
@@ -282,6 +321,9 @@ rightToMaybe =
 
 {-| Convert from a `Maybe` to `Either` with a default value
 for `Left` for `Nothing`.
+
+    fromMaybe "Hello" <| Just 2 == Right 2
+    fromMaybe "Hello" Nothing == Left "Hello"
 -}
 fromMaybe : a -> Maybe b -> Either a b
 fromMaybe d m =
@@ -295,6 +337,9 @@ fromMaybe d m =
 
 {-| Convert from a `Maybe` to `Either` with a default value
 for `Right` for `Nothing`.
+
+    leftFromMaybe 3 <| Just "World" == Left "World"
+    leftFromMaybe 3 Nothing == Right 3
 -}
 leftFromMaybe : b -> Maybe a -> Either a b
 leftFromMaybe d m =
@@ -318,6 +363,9 @@ rightFromMaybe =
 
 
 {-| Convert from `Either` to `Result`.
+
+    toResult <| Left "World" == Err "World"
+    toResult <| Right 2 == Ok 2
 -}
 toResult : Either a b -> Result a b
 toResult e =
@@ -330,6 +378,9 @@ toResult e =
 
 
 {-| Convert from `Result` to `Either`.
+
+    fromResult <| Err "World" == Left "World"
+    fromResult <| Ok 2 == Right 2
 -}
 fromResult : Result a b -> Either a b
 fromResult r =
@@ -346,6 +397,9 @@ fromResult r =
 
 
 {-| Returns `True` if argument is `Left _`
+
+    isLeft <| Left "World" == True
+    isLeft <| Right 2 == False
 -}
 isLeft : Either a b -> Bool
 isLeft e =
@@ -358,6 +412,9 @@ isLeft e =
 
 
 {-| Returns `True` if argument is `Right _`
+
+    isRight <| Left "World" == False
+    isRight <| Right 2 == True
 -}
 isRight : Either a b -> Bool
 isRight e =
@@ -370,6 +427,9 @@ isRight e =
 
 
 {-| Extract left value or a default.
+
+    fromLeft "World" <| Left "Hello" == "Hello"
+    fromLeft "World" <| Right 2 == "World"
 -}
 fromLeft : a -> Either a b -> a
 fromLeft d e =
@@ -382,6 +442,9 @@ fromLeft d e =
 
 
 {-| Extract right value or a default.
+
+    fromRight 3 <| Left "Hello" == 3
+    fromRight 3 <| Right 2 == 2
 -}
 fromRight : b -> Either a b -> b
 fromRight d e =
@@ -402,6 +465,9 @@ withDefault =
 
 {-| Given a function for both `Left` and `Right` to to type a generic
 type `c`, collapse down the `Either` to a value of that type.
+
+    elim identity toString <| Left "World" == "World"
+    elim identity toString <| Right 2 == "2"
 -}
 elim : (a -> c) -> (b -> c) -> Either a b -> c
 elim f g e =
@@ -421,6 +487,9 @@ either =
 
 
 {-| Swap the `Left` and `Right` sides of an `Either`.
+
+    swap <| Left "World" == Right "World"
+    swap <| Right 2 == Left 2
 -}
 swap : Either a b -> Either b a
 swap e =
